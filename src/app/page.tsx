abdd55599c6e08b539627db6c920cf52868cbe8d
Sixtus Agbakwuru <sixtus.agbakwuru1@gmail.com>
2024-07-26 01:13:36 -0700

    Home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import Image from "next/image";
+import Form from "./(university-of-lagos-unilag)/unilag/postgraduate/form/form";
+import { NavigationMenuDemo } from "@/components/navigation-bar";
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <h1>Educational Blog and News</h1>
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
+    <div className = "bg-vasaskills-blue">
+            <header className = "grid grid-cols-1 sticky top-0 shadow md:grid-cols-12  mx-2 md:mx-32">
+                <div className="md:col-span-4 bg-white pe-4 ps-4 flex justify-center md:justify-start">
+                    <Image src = "/vasaskills-logo.png" alt = "Vasaskills Institute Logo" width = {65} height = {65} className="max-w-full h-auto"/>
+                </div>
+                <div className="md:col-span-8 p-4 bg-white flex items-center justify-end">
+                <NavigationMenuDemo />
+                {/*
+                <Navbar />
+                */}
+                </div>
+            </header>
+            <div className = "grid grid-cols-1 md:grid-cols-12 gap-8 my-8 mx-2 md:mx-32">
+            <main className = "md:col-span-8 bg-white rounded">
+                <Form />
 
-      </main>
-    </div>
+            </main>
+            <aside className = "md:col-span-4 bg-white rounded">
+                <div className = "rounded p-4 w-full">
+                    <h2 className = "text-2xl font-bold">Recent Posts</h2>
+
+                </div>
+                <h2>Side Bar</h2>
+            </aside>
+            </div>
+            <footer>
+
+            </footer>
+
+        </div>
   );
 }
